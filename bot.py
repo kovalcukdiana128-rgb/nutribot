81fc0cd6fbc2f31c3d302dbe0efa0c1e8cb31742
kovalcukdiana128-rgb <kovalcukdiana128@gmail.com>
2026-05-21 16:58:20 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -19,7 +19,6 @@ from google.oauth2 import service_account
 # =========================
 # 🔑 ENV
 # =========================
-TOKEN = os.getenv("8635120471:AAH-jXEj5Lw6pVpH6mxc-7nIfu0h9fhx8p4")
 
 from google.cloud import vision
 from google.oauth2 import service_account
